f656f662e500bdf60d0b9ac64297662ee4a8653b
dsrules007 <sousilvaney@gmail.com>
2020-10-16 10:50:39 +0100

    test routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,10 @@
 import express from 'express'
 
 const app = express();
+app.use(express.json())
+
+app.get('/users/',(request,response)=>{
+response.json({ message:'hello'});  
+})
 
 app.listen(3333);
