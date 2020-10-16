5e4f678e7ae0fd3eb1e64bfe81b1212ef1e2f568
dsrules007 <sousilvaney@gmail.com>
2020-10-16 13:29:44 +0100

    create controll and route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express'
 import './database/connection'
+import Routes from './Routes'
 
 const app = express();
 app.use(express.json())
+app.use(Routes);
 
-app.get('/users/',(request,response)=>{
-response.json({ message:'hello'});  
-})
 
 app.listen(3333);
